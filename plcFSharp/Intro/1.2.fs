2c134019065150380a379ffc06afdd5549ab7b9c
Madeleine <majak@itu.dk>
2026-08-27 12:08:28 +0200

    1.2 IV Not finished

--- plcFSharp/Intro/1.2.fs
+++ plcFSharp/Intro/1.2.fs
@@ -27,3 +27,20 @@ let rec fmt a =
         " (" + fmt a1 + " - " + fmt a2 + ") " 
     | Mul (a1, a2) -> 
         " (" + fmt a1 + " * " + fmt a2 + ") " ;;
+
+//IV
+let rec simplify a =
+    match a with
+    | CstI i -> CstI i
+    | Var v -> Var v
+    | Add (CstI 0, a1) -> simplify a1
+    | Add (a1, CstI 0) -> simplify a1
+    | Sub (a1, CstI 0) -> simplify a1
+    | Mul (CstI 1, a1) -> simplify a1
+    | Mul (a1, CstI 1) -> simplify a1
+    | Mul (CstI 0, a1) -> CstI 0
+    | Mul ( a1, CstI 0) -> CstI 0
+    | Sub (a1, a2) when a1 = a2 -> CstI 0
+    | Add (a1, a2) -> simplify (Add (simplify a1, simplify a2))
+    | Mul (a1, a2) -> simplify (Mul (simplify a1, simplify a2))
+    | Sub (a1, a2) -> simplify (Sub (simplify a1, simplify a2))
