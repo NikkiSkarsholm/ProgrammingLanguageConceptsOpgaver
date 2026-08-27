c058b0e1a004f5c8674e7179c0e65d0a2c4ff6fc
Nikki <nikki.s.risager@gmail.com>
2026-08-27 11:36:55 +0200

    1.2 II

---
+++ plcFSharp/Intro/1.2.fs
@@ -0,0 +1,17 @@
+﻿
+let env = [("a", 3); ("c", 78); ("baf", 666); ("b", 111)];;
+
+let emptyenv = []; (* the empty environment *)
+
+// I
+type aexpr = 
+    | CstI of int
+    | Var of string 
+    | Add of aexpr * aexpr
+    | Mul of aexpr * aexpr
+    | Sub of aexpr * aexpr;;
+
+// II
+let e1 = Sub(Var "v", Add(Var "w", Var "z"));;
+let e2 = Mul(CstI 2, Sub(Var "v", Add(Var "w", Var "z")));;
+let e3 = Add(Var "x", Add(Var "y", Add(Var "z", Var "v")));;
