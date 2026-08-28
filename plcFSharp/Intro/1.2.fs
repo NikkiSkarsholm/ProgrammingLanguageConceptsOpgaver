0031db630f78e1a5645c736266e528d6b0254076
Nikki <nikki.s.risager@gmail.com>
2026-08-28 12:50:07 +0200

    Fixed 1.2 IV

--- plcFSharp/Intro/1.2.fs
+++ plcFSharp/Intro/1.2.fs
@@ -41,6 +41,21 @@ let rec simplify a =
     | Mul (CstI 0, a1) -> CstI 0
     | Mul ( a1, CstI 0) -> CstI 0
     | Sub (a1, a2) when a1 = a2 -> CstI 0
-    | Add (a1, a2) -> simplify (Add (simplify a1, simplify a2))
-    | Mul (a1, a2) -> simplify (Mul (simplify a1, simplify a2))
-    | Sub (a1, a2) -> simplify (Sub (simplify a1, simplify a2))
+    | Add (a1, a2) -> 
+        let a1s = simplify a1 
+        let a2s = simplify a2
+        if a1s = a1 && a2s = a2 
+        then Add(a1s, a2s)
+        else simplify (Add(a1s, a2s))
+    | Mul (a1, a2) -> 
+        let a1s = simplify a1 
+        let a2s = simplify a2
+        if a1s = a1 && a2s = a2 
+        then Mul(a1s, a2s)
+        else simplify (Mul(a1s, a2s))
+    | Sub (a1, a2) -> 
+        let a1s = simplify a1 
+        let a2s = simplify a2
+        if a1s = a1 && a2s = a2 
+        then Sub(a1s, a2s)
+        else simplify (Sub(a1s, a2s))
